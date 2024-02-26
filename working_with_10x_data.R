ce82cfd147cb86105697bc0021f0cabe8e895a5d
Rhodes.Ford@ucsf.edu <rhodes.ford@ucsf.edu>
2024-02-26 11:33:09 -0800

    Updates in working_with_10x_data.R. Functions working now

--- working_with_10x_data.R
+++ working_with_10x_data.R
@@ -26,7 +26,7 @@
 ## Input (option 2): A single directory with multiple samples each having 
 ##   a corresponding *matrix.mtx.gz, *barcodes.tsv.gz, and *genes.tsv.gz file 
 ##
-## Outputs: Found in the following directories
+## Outputs: Seurat objects for each sample included in 10x dataset
 ##   
 ##    
 ##
@@ -47,12 +47,36 @@ library(patchwork)
 
 ## Define Functions
 
+FileParsing10xDirs <- function(directory){
+  
+  file.list <- list.files(directory)
+  
+  dir.contents <- lapply(file.list, 
+                         function(x) paste(directory, x, sep = "/"))
+  
+  # Make list to hold Seurat objects
+  seurat.obj.list <- list()
+  
+  for (i in 1:length(dir.contents)) {
+    # Make the 10x object
+    obj.10x <- Read10X(data.dir = paste(dir.contents[i], "/", sep = ""))
+    
+    # Initialize the Seurat object with the raw (non-normalized data).
+    obj.seurat <- CreateSeuratObject(counts = obj.10x, project = file.list[i], min.cells = 3, min.features = 200)
+    
+    # put seurat object into a list
+    seurat.obj.list[[i]] <- obj.seurat
+  }
+  return(seurat.obj.list)
+}
+
+
 FileParsing <- function(directory) {
   # directory can include either other directories, with each of the three necessary 10x files or it can have multiple named versions of these files
   
-  dir.contents <- list.files(directory)
+  file.list <- list.files(directory)
   
-  dir.contents <- lapply(dir.contents, 
+  dir.contents <- lapply(file.list, 
                          function(x) paste(directory, x, sep = "/"))
   
   dir.bool <- unlist(lapply(dir.contents, 
@@ -64,47 +88,78 @@ FileParsing <- function(directory) {
   print(dir.contents)
   
   if (length(dir.contents[dir.bool]) == length(dir.contents)) {
-
-    print("There are only directories in this parent directory. Analysis begun.")
-
-    seuratObjList <- list()
-
-    for (i in 1:length(dir.contents)) {
-      # Load the PBMC dataset
-      obj.10x <- Read10X(data.dir = paste(dir.contents[i], "/", sep = ""))
-      # Initialize the Seurat object with the raw (non-normalized data).
-      obj.seurat <- CreateSeuratObject(counts = obj.10x, project = filelist[i], min.cells = 3, min.features = 200)
-      seurat.obj.list[[i]] <- obj.seurat
-      }
+    
+    print("There are only directories in this parent directory. Directory analysis begun.")
+    seurat.obj.list <- FileParsing10xDirs(directory)
+    
   } else if (length(dir.contents[file.bool]) == length(dir.contents)) {
-      print("There are only files in this parent directory. Analysis begun.")
-
-      samplelist <- dir.contents[grep(pattern = "matrix.mtx.gz", x = dir.contents)]
-      filepattern <- gsub("_matrix.mtx.gz", "", samplelist)
-
-      print("The following samples are present in the provided directory:")
-      print(filepattern)
-
-      for (i in 1:length(filepattern)) {
-
-      }
-
-    seuratObjList <- list()
-    } else {
-      stop("There are both files and directories in this parent. Please make sure file organization is consistent with either mtx, barcodes, and genes files in distinct directories or named and found in parent directory with no other directories present")
+    print("There are only files in this parent directory. Analysis begun.")
+    
+    # Get list of matrix, barcode, and genes files
+    sample.list.mat <- file.list[grep(pattern = "matrix.mtx.gz", x = file.list)]
+    sample.list.mat <- gsub("_matrix.mtx.gz", "", sample.list.mat)
+    
+    sample.list.bar <- file.list[grep(pattern = "barcodes.tsv.gz", x = file.list)]
+    sample.list.bar <- gsub("barcodes.tsv.gz", "", sample.list.bar)
+    
+    if(length(file.list[grep(pattern = "genes.tsv.gz", x = file.list)]) != 0){
+      sample.list.genes <- file.list[grep(pattern = "genes.tsv.gz", x = file.list)]
+      sample.list.genes <- gsub("genes.tsv.gz", "", sample.list.genes)
+    }else if(length(file.list[grep(pattern = "features.tsv.gz", x = file.list)]) != 0){
+      sample.list.genes <- file.list[grep(pattern = "features.tsv.gz", x = file.list)]
+      sample.list.genes <- gsub("features.tsv.gz", "", sample.list.genes)
     }
-    return(seuratObjList)
+    
+    # Make sure lengths of the lists of each file type are the same length (all files provided)
+    if(length(sample.list.mat) == length(sample.list.bar) & length(sample.list.mat) == length(sample.list.genes)){
+      sample.list <- sample.list.mat
+    }else{
+      stop("Not all samples included in this directory have all three necessary files. Please make sure to include all three necessary files for each sample")
+    }
+    
+    print("The following samples are present in the provided directory with all three 10x files:")
+    print(sample.list)
+    
+    # Make new parent directory
+    new.parent.dir <- paste(directory, '_matched_files', sep ="")
+    dir.create(new.parent.dir)
+    
+    for (i in 1:length(sample.list)) {
+      # Get the three 10x files for sample i
+      sample.files <- file.list[grepl(pattern = sample.list[i],x = file.list)]
+      
+      # Make new directory containing the three 10x files for that sample
+      new.dir <- paste(new.parent.dir,sample.list[i],sep = "/")
+      dir.create(new.dir)
+      
+      # Move three 10x files into the new directory 
+      file.rename(paste(directory,sample.files, sep = "/"), paste(new.dir, sample.files, sep='/'))
+      
+      newdir.file.list <- list.files(new.dir)
+      if(length(newdir.file.list) != 3){
+        stop("Something has gone wrong. The three necessary 10x files were not transferred properly")
+      }else{
+        newdir.file.path <- paste(new.dir,newdir.file.list,sep = '/')
+        for(j in 1:length(newdir.file.path)){
+          if(grepl('barcodes.tsv.gz',newdir.file.path[j])){
+            file.rename(newdir.file.path[j],paste(new.dir,'barcodes.tsv.gz', sep = "/"))
+          }else if(grepl('matrix.mtx.gz',newdir.file.path[j])){
+            file.rename(newdir.file.path[j],paste(new.dir,'matrix.mtx.gz', sep = "/"))
+          }else if(grepl('genes.tsv.gz',newdir.file.path[j])){
+            file.rename(newdir.file.path[j],paste(new.dir,'features.tsv.gz', sep = "/"))
+          }else if(grepl('features.tsv.gz',newdir.file.path[j])){
+            file.rename(newdir.file.path[j],paste(new.dir,'features.tsv.gz', sep = "/"))
+          }
+        }
+      }
+    }
+    # Run directory analysis with new directory-separated data
+    seurat.obj.list <- FileParsing10xDirs(new.parent.dir)
+    
+  } else {
+    stop("There are both files and directories in this parent. Please make sure file organization is consistent with either mtx, barcodes, and genes files in distinct directories or named and found in parent directory with no other directories present")
+  }
+  return(seurat.obj.list)
 }
 
 ## ---------------------------
-
-## Main (Executed statements)
-
-# Set working directory
-setwd("/c4/home/brford/breastCancerProject/")
-
-
-# Run functions
-gse206638 <- seuratProcessing("GSE206638_RAW/")
-gse114724 <- seuratProcessing("GSE114724_RAW")
-
